99056ad9f0f5d35d9e132e84781888dc40cc84c9
orison-j <obuye37@gmail.com>
2025-02-19 22:51:47 +0100

    minor edits on donate page

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -17,7 +17,7 @@ const Impacts = () => {
   const BSCAddress = 'ewuhfehhfhepoiepjijiejpqjoijpqijproi'
 
   const copyAddress = (AddrRef:RefObject<HTMLParagraphElement | null>) => {
-    const textToCopy = AddrRef.current?.innerText
+    const textToCopy = AddrRef.current?.getAttribute("data-value")
     if(!textToCopy) return {success:false, text:''}
       navigator.clipboard.writeText(textToCopy)
       return {success:true, text:textToCopy}
@@ -62,7 +62,7 @@ const Impacts = () => {
       <div className='relative text-center flex justify-center items-center gap-2 flex-wrap md:px-10' >
         <div className={`relative border-1 p-2 sm:p-8 flex flex-col justify-center items-center w-full sm:w-[30%] ${copySuccess.success ? `before:w-auto before:bg-gray-950 before:text-amber-50 ${copySuccess.text === SolAddress ? "before:content-['copied']" : ""} before:mx-2 before:absolute before:px-2 before:py-1 before:h-auto before:top-0 before:right-10` : ""}`}>
           <h1 className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-linear-to-r before:from-transparent before:to-indigo-200/50 after:h-px after:w-8 after:bg-linear-to-l after:from-transparent after:to-indigo-200/50" inert>SOLANA(SOL)</h1>
-          <p ref={SolRef}>{SolAddress.slice(0, 8)} ..... {SolAddress.slice(-8)}</p>
+          <p ref={SolRef} data-value={SolAddress}>{SolAddress.slice(0, 8)} ..... {SolAddress.slice(-8)}</p>
           
           <Image
           priority
@@ -93,7 +93,7 @@ const Impacts = () => {
           }}
           />
           <h1 className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-linear-to-r before:from-transparent before:to-indigo-200/50 after:h-px after:w-8 after:bg-linear-to-l after:from-transparent after:to-indigo-200/50" inert>ETHEREUM(ETH)</h1>
-          <p ref={EthRef}> {EthAddress.slice(0, 8)} ..... {EthAddress.slice(-8)} </p> 
+          <p ref={EthRef} data-value={EthAddress}> {EthAddress.slice(0, 8)} ..... {EthAddress.slice(-8)} </p> 
 
         </div>
 
@@ -111,7 +111,7 @@ const Impacts = () => {
           }}
           />
           <h1 className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-linear-to-r before:from-transparent before:to-indigo-200/50 after:h-px after:w-8 after:bg-linear-to-l after:from-transparent after:to-indigo-200/50" inert>BINANCE SMART CHAIN(BSC) </h1>
-          <p ref={BSCRef}>{BSCAddress.slice(0, 8)} ..... {BSCAddress.slice(-8)}</p>
+          <p ref={BSCRef} data-value={BSCAddress}>{BSCAddress.slice(0, 8)} ..... {BSCAddress.slice(-8)}</p>
         </div>  
       </div>
       
