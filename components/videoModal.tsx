ebdf8548ed00f98cf7eca0426fcea927dbbcf925
obuye37 <bomajehori@gmail.com>
2025-03-23 19:21:46 +0100

    added video iframe size to impact page

--- components/videoModal.tsx
+++ components/videoModal.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import useWindowSize from '@rooks/use-window-size';
 
 interface VideoModalProps {
   name:string;
@@ -7,9 +8,12 @@ interface VideoModalProps {
 
 
 const VideoModal = ({name, video}: VideoModalProps) => {
+  const {innerWidth, innerHeight} = useWindowSize()
+  const screenWidth = innerWidth != null ? innerWidth : 400
+  
   return (  
     <div className='max-w-[80%] max-h-[80%] flex flex-col justify-center items-center'>
-      <iframe width="800" height="600" src={`https://www.youtube.com/embed/${video}`} title={`${name}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
+      <iframe width={screenWidth < 720 ? "500" : "800"} height={screenWidth < 720 ? "300" : "500"} src={`https://www.youtube.com/embed/${video}`} title={`${name}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
     </div>
    
   ) 
