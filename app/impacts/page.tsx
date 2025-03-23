a175b91216715bef79725b6335c5b541c89a3433
obuye37 <bomajehori@gmail.com>
2025-03-23 23:53:52 +0100

    moved stats comp. to bottom

--- app/impacts/page.tsx
+++ app/impacts/page.tsx
@@ -23,25 +23,27 @@ const handleModalVideo = (name:string, videSrc:string) => {
 
   return (
     <>
-      <div className='relative flex flex-wrap gap-2 justify-around items-center p-10 bg-[url("/images/dotBg.webp")] bg-cover bg-center h-[90svh]'>
-      {videos.map(({name, desc, thumb, vidSrc}, idx) => (
-      <div key={idx} className='flex flex-col items-center relative p-1.5 border-1 border-amber-300/80 rounded-2xl overflow-hidden cursor-pointer' onClick={() => handleModalVideo(name, vidSrc) }>
-        <Image 
-          src={thumb.imgSrc}
-          alt={thumb.imgAlt}
-          width={250}
-          height={250}
-          className='place-self-center rounded-2xl'
-        />
-        <h3 className={`${!name && "absolute h-full translate-[50%] left-0 top-0"}`}>{name ? name : "No file uploaded yet"}</h3>
-        <p>{desc}</p>
+      <div className='relative flex flex-wrap gap-2 justify-around items-center p-10 bg-[url("/images/dotBg.webp")] bg-cover sm:h-screen bg-center'>
+        {videos.map(({name, desc, thumb, vidSrc}, idx) => (
+        <div key={idx} className='flex flex-col items-center relative p-1.5 border-1 border-amber-300/80 rounded-2xl overflow-hidden cursor-pointer' onClick={() => handleModalVideo(name, vidSrc) }>
+          <Image 
+            src={thumb.imgSrc}
+            alt={thumb.imgAlt}
+            width={250}
+            height={250}
+            className='place-self-center rounded-2xl'
+          />
+          <h3 className={`${!name && "absolute h-full translate-[50%] left-0 top-0"}`}>{name ? name : "No file uploaded yet"}</h3>
+          <p>{desc}</p>
+        </div>
+      ))}
+      <div className={`${modalOpen ? "flex" : "hidden"} absolute w-full h-full bg-gray-950/85 justify-center items-center top-0`} onClick={()=>setModalOpen(false)}>
+        <VideoModal name={videoName} video={vidSrc} pauseVid={modalOpen ? "pause" : ""} />
       </div>
-    ))}
-    <div className={`${modalOpen ? "flex" : "hidden"} absolute w-full h-full bg-gray-950/85 justify-center items-center top-0`} onClick={()=>setModalOpen(false)} >
-      <VideoModal name={videoName} video={vidSrc} />
     </div>
-    </div>
+
     <hr className='my-3'/>
+    
     <Statistics />
       
     </>
--- components/videoModal.tsx
+++ components/videoModal.tsx
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import useWindowSize from '@rooks/use-window-size';
 
 interface VideoModalProps {
   name:string;
   video:string;
+  pauseVid: string;
 }
 
 
-const VideoModal = ({name, video}: VideoModalProps) => {
-  const {innerWidth, innerHeight} = useWindowSize()
+const VideoModal = ({name, video, pauseVid}: VideoModalProps) => {
+  const {innerWidth} = useWindowSize()
   const screenWidth = innerWidth != null ? innerWidth : 400
+
+  const iframeRef = useRef<HTMLIFrameElement>(null)
+
+  useEffect(() => {
+    const pauseVideo = () => {
+      console.log("effect ran ", pauseVid)
+      if(pauseVid == 'pause') {
+        if (iframeRef.current) {
+          console.log(iframeRef.current.contentWindow)
+        const video = iframeRef.current.contentWindow?.postMessage('pause', "*" ) // document.querySelector('video');
+        // if (video) {
+        //   video.pause();
+        // }
+      }
+      }
+    }
+    return () => {
+      pauseVideo()
+    }
+  }, [pauseVid])
+  
+ 
   
   return (  
     <div className='max-w-[80%] max-h-[80%] flex flex-col justify-center items-center'>
-      <iframe width={screenWidth < 720 ? "500" : "800"} height={screenWidth < 720 ? "300" : "500"} src={`https://www.youtube.com/embed/${video}`} title={`${name}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
+      <iframe ref={iframeRef} width={screenWidth < 720 ? "500" : "800"} height={screenWidth < 720 ? "300" : "500"} src={`https://www.youtube.com/embed/${video}?version=3&enablejsapi=1`} title={`${name}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
     </div>
    
   ) 
